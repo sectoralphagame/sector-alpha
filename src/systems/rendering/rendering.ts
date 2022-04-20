a6eb424e465686bc54a27a318bf37a56fea66c5e
dominikzeglen <flesz3@o2.pl>
2022-04-20 14:22:04 +0200

    Add basic pixi renderer

--- src/systems/rendering/rendering.ts
+++ src/systems/rendering/rendering.ts
@@ -1,9 +1,12 @@
 import P5 from "p5";
-import Color from "color";
 import { Sim } from "../../sim";
 import "./components/Panel";
+import * as PIXI from "pixi.js";
+import { SVGScene } from "@pixi-essentials/svg";
+import { Viewport } from "pixi-viewport";
 import { System } from "../system";
-import { RenderCamera, zMin } from "./camera";
+import f_civ from "../../../assets/f_shipyard.svg";
+import s_civ from "../../../assets/s_civ.svg";
 import { Query } from "../query";
 
 export class RenderingSystem extends System {
@@ -22,84 +25,66 @@ export class RenderingSystem extends System {
   }
 
   init = () => {
-    this.p5 = new P5((p5: P5) => {
-      let camera: RenderCamera;
-      const settingsEntity = this.sim.queries.selectionManager.get()[0];
+    let selected = null;
 
-      p5.setup = () => {
-        const canvas = p5.createCanvas(
-          window.innerWidth,
-          window.innerHeight,
-          "webgl"
-        );
-        canvas.parent(this.parent);
-        camera = new RenderCamera(p5);
-      };
-
-      p5.draw = () => {
-        if (settingsEntity.cp.selectionManager.focused) {
-          camera.lookAt(
-            settingsEntity.cp.selectionManager.entity.cp.position.x,
-            settingsEntity.cp.selectionManager.entity.cp.position.y
-          );
-        }
-        p5.background("black");
-
-        this.renderable.get().forEach((entity) => {
-          const selected = settingsEntity.cp.selectionManager.entity === entity;
-          if (camera.scale < entity.cp.render.minScale && !selected) return;
-
-          const baseColor =
-            entity.cp.render.color ?? entity.cp.owner?.value.color ?? "#dddddd";
-          const color =
-            settingsEntity.cp.selectionManager.entity === entity
-              ? Color(baseColor).lighten(0.2).unitArray()
-              : Color(baseColor).unitArray();
-          p5.fill(color[0] * 256, color[1] * 256, color[2] * 256);
-          p5.noStroke();
-          p5.circle(
-            entity.cp.position.x * 10,
-            entity.cp.position.y * 10,
-            (camera.z / zMin) * (selected ? 1.3 : 1) * entity.cp.render.size
-          );
-        });
-      };
-
-      p5.mouseWheel = (event: { delta: number }) => {
-        camera.move({
-          z: event.delta,
-        });
-      };
-
-      p5.mouseDragged = (event: MouseEvent) => {
-        settingsEntity.cp.selectionManager.focused = false;
-        camera.move({
-          x: (event.movementX * camera.z) / zMin / 10,
-          y: (event.movementY * camera.z) / zMin / 10,
-        });
-      };
-
-      p5.mouseClicked = () => {
-        const clickables = this.selectable.get().filter((e) => e.cp.selection);
-        const clicked = clickables.find((entity) => {
-          const [x, y] = camera.translateScreenToCanvas(p5.mouseX, p5.mouseY);
-          return (
-            (entity.cp.position.x * 10 - x) ** 2 +
-              (entity.cp.position.y * 10 - y) ** 2 <=
-            (camera.z / zMin) * entity.cp.render.size * 2
-          );
-        });
-
-        if (clicked) {
-          settingsEntity.cp.selectionManager.set(clicked);
-        }
-      };
-
-      p5.windowResized = () => {
-        p5.resizeCanvas(this.parent.clientWidth, this.parent.clientHeight);
-        camera.updateViewport();
-      };
+    const app = new PIXI.Application({
+      antialias: true,
+      autoDensity: true,
+      resolution: window.devicePixelRatio,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      view: root,
     });
+
+    const viewport = new Viewport({
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+      interaction: app.renderer.plugins.interaction,
+    });
+
+    app.stage.addChild(viewport);
+
+    viewport.drag().pinch().wheel();
+
+    const textures = {
+      sCiv: PIXI.Texture.from(s_civ),
+      fCiv: PIXI.Texture.from(f_civ),
+    };
+
+    const sprite = new PIXI.Sprite(textures.sCiv);
+    sprite.tint = 0xff0;
+    sprite.anchor.set(0.5);
+    sprite.position.set(500, 500);
+    sprite.scale.set(0.8);
+
+    viewport.addChild(sprite);
+
+    const spriteF = new PIXI.Sprite(textures.fCiv);
+    spriteF.tint = 0xff0;
+    spriteF.anchor.set(0.5);
+    spriteF.position.set(600, 500);
+    spriteF.interactive = true;
+    spriteF.scale.set(1.5);
+
+    spriteF.on("pointerover", () => {
+      console.log("oh");
+      selected = 1;
+    });
+    spriteF.on("pointerout", () => {
+      selected = null;
+    });
+
+    app.ticker.add(() => {
+      if (selected) {
+        spriteF.scale.set(
+          spriteF.scale.x + (spriteF.scale.x > 1.9 ? -0.02 : 0.01)
+        );
+      } else {
+        spriteF.scale.set(1.5);
+      }
+    });
+
+    viewport.addChild(spriteF);
   };
 
   // eslint-disable-next-line no-unused-vars, class-methods-use-this
