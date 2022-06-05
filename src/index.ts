2226ad7a53cd518b231a5424f1e73697b42e2dcf
dominikzeglen <flesz3@o2.pl>
2022-06-06 00:06:06 +0200

    Fix typos

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-// import world from "./world";
-// import { Sim } from "./sim";
-// import "./style";
-
-// const sim = new Sim();
-// sim.init();
-
-// window.sim = sim;
-// world(sim);
-// sim.start();
-
+import world from "./world";
 import { Sim } from "./sim";
 import "./style";
 
-const sim = Sim.load();
+const sim = new Sim();
+sim.init();
 
 window.sim = sim;
+world(sim);
 sim.start();
+
+// import { Sim } from "./sim";
+// import "./style";
+
+// const sim = Sim.load();
+
+// window.sim = sim;
+// sim.start();
