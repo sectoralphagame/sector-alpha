dd030b7e9c1c078005a6d07a660c8de8f7e978eb
dominikzeglen <flesz3@o2.pl>
2022-07-07 02:20:37 +0200

    Fix teleport placement

--- src/world/factions.ts
+++ src/world/factions.ts
@@ -1,18 +1,11 @@
 import { add, Matrix, matrix, random } from "mathjs";
-import {
-  createFaction,
-  Faction,
-  faction as asFaction,
-} from "../archetypes/faction";
-import { Sector, sectorSize } from "../archetypes/sector";
+import { createFaction, Faction } from "../archetypes/faction";
+import { sectorSize } from "../archetypes/sector";
 import { createShip } from "../archetypes/ship";
 import { setMoney } from "../components/budget";
 import { hecsToCartesian } from "../components/hecsPosition";
-import { createRenderGraphics } from "../components/renderGraphics";
-import { linkTeleportModules } from "../components/teleport";
 import { Sim } from "../sim";
 import { getFreighterTemplate } from "../systems/shipPlanning";
-import { createTeleporter } from "./facilities";
 
 function createTerritorialFaction(index: number, sim: Sim) {
   const char = String.fromCharCode(index + 65);
@@ -32,41 +25,6 @@ function createTradingFaction(index: number, sim: Sim) {
   return faction;
 }
 
-function createLink(sim: Sim, sectors: Sector[]) {
-  const [telA, telB] = sectors.map((sector) => {
-    const position = hecsToCartesian(
-      sector.cp.hecsPosition.value,
-      sectorSize / 10
-    );
-
-    const teleporter = sim
-      .getOrThrow(
-        createTeleporter(
-          {
-            owner: sector.cp.owner
-              ? asFaction(sim.getOrThrow(sector.cp.owner.id))
-              : undefined!,
-            position: add(
-              position,
-              matrix([
-                random(-sectorSize / 20, sectorSize / 20),
-                random(-sectorSize / 20, sectorSize / 20),
-              ])
-            ) as Matrix,
-            sector,
-          },
-          sim
-        ).cp.modules.ids[0]
-      )
-      .requireComponents(["teleport"]);
-
-    return teleporter;
-  });
-  telA.addComponent(createRenderGraphics("link"));
-
-  linkTeleportModules(telA, telB);
-}
-
 let faction: Faction;
 
 export const factions = (sim: Sim) => {
@@ -78,13 +36,6 @@ export const factions = (sim: Sim) => {
         : faction;
     sector.addComponent({ name: "owner", id: faction.id });
   });
-
-  for (let i = 2; i < sectors.length; i++) {
-    createLink(sim, [sectors[i - 1], sectors[i]]);
-  }
-
-  createLink(sim, [sectors[0], sectors[2]]);
-  createLink(sim, [sectors[0], sectors[7]]);
 
   for (let i = 0; i < 2; i++) {
     faction = createTradingFaction(i, sim);
