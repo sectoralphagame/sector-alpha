95e5d3515d127b925e45c8046e72e72ff4b4cb4d
dominikzeglen <flesz3@o2.pl>
2023-02-16 22:49:38 +0100

    Add config files

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+import custom from "../webpack.config.js";
+
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../ui/**/*.stories.tsx",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [...config.module.rules, ...custom.module.rules],
+      },
+      resolve: {
+        ...config.resolve,
+        plugins: custom.resolve.plugins,
+      },
+    };
+  },
+};
