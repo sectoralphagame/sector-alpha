64fa9a9319018fd5f7dd3b38c41c04dda8d235fb
dominikzeglen <flesz3@o2.pl>
2023-04-20 23:51:29 +0200

    Fix reward type name mismatch

--- core/systems/missionTracking/rewards.ts
+++ core/systems/missionTracking/rewards.ts
@@ -26,7 +26,7 @@ export interface RelationReward {
 }
 
 export function isRelationReward(reward: Reward): reward is RelationReward {
-  return reward.type === "relations";
+  return reward.type === "relation";
 }
 
 export function relationRewardHandler(reward: Mission, sim: Sim): void {
