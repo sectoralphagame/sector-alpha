fe665427b3740b64105f8163d4ba279a4730441d
dominikzeglen <flesz3@o2.pl>
2024-05-25 14:39:52 +0200

    Add error boundary

--- gateway/index.tsx
+++ gateway/index.tsx
@@ -6,11 +6,16 @@ import { Root } from "@ui/index";
 import { isDev } from "@core/settings";
 import Bugsnag from "@bugsnag/js";
 import BugsnagPluginReact from "@bugsnag/plugin-react";
+import { ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
 import packageJson from "../package.json";
 
-let ErrorBoundary: React.FC<React.PropsWithChildren<{}>> = ({ children }) =>
-  // @ts-expect-error
-  children;
+let ErrorBoundary: React.FC<
+  React.PropsWithChildren<{ FallbackComponent: React.FC; onError: () => void }>
+> = ({ children, FallbackComponent, onError }) => (
+  <ReactErrorBoundary fallbackRender={FallbackComponent} onError={onError}>
+    {children}
+  </ReactErrorBoundary>
+);
 
 if (process.env.BUGSNAG_API_KEY) {
   Bugsnag.start({
@@ -26,8 +31,25 @@ if (process.env.BUGSNAG_API_KEY) {
 const root = createRoot(document.querySelector("#root")!);
 const DevTools = isDev ? React.lazy(() => import("../devtools")) : () => null;
 
+const ErrorView: React.FC = () => (
+  <div id="error">
+    <div>
+      <h3>Something went wrong :(</h3>
+      <p>
+        Try reloading the page, or contact{" "}
+        <a href="https://discord.gg/2PnbGRYe">our support</a>.
+      </p>
+    </div>
+  </div>
+);
+
 root.render(
-  <ErrorBoundary>
+  <ErrorBoundary
+    FallbackComponent={ErrorView}
+    onError={() => {
+      window.sim?.stop();
+    }}
+  >
     <Styles>
       <BrowserRouter>
         <Routes>
