fb72c9c136febde49c3385cc3237c1b80f85de8d
Dominik Żegleń <flesz3@o2.pl>
2024-11-21 22:58:45 +0100

    Simplify code

--- ogl-engine/materials/skybox/shader.frag.glsl
+++ ogl-engine/materials/skybox/shader.frag.glsl
@@ -8,7 +8,6 @@ in vec3 vDir;
 out vec4 fragData[2];
 
 void main() {
-    fragData[0].rgb = texture(tMap, vDir).rgb;
-    fragData[0].a = 1.0f;
+    fragData[0] = texture(tMap, vDir);
     fragData[1] = vec4(0.f);
 }
