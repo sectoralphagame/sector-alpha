9a860f97af3e6a8af6ba82396805a79f3447c96f
Dominik Żegleń <flesz3@o2.pl>
2024-12-06 01:50:00 +0100

    Add ui:arrow tag

--- core/tags.ts
+++ core/tags.ts
@@ -19,6 +19,7 @@ const tags = [
   "ai:attack-force",
   "ai:spare",
   "ai:mission",
+  "ui:arrow",
   ...shipRoles.map<`role:${ShipRole}`>((role) => `role:${role}`),
   ...modules.map<`facilityModuleType:${FacilityModuleType}`>(
     ({ type: facilityModuleType }) =>
