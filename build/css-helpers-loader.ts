6afa4a2467213001628dc5c77291b0612a892e99
Dominik Żegleń <flesz3@o2.pl>
2024-12-23 00:52:48 +0100

    Change base size to 10

--- build/css-helpers-loader.ts
+++ build/css-helpers-loader.ts
@@ -4,7 +4,7 @@ import type webpack from "webpack";
 
 // eslint-disable-next-line func-names
 const loader: webpack.LoaderDefinition = function (this, content) {
-  return content.replace(/usesize\((.+)\)/g, "calc($1 * usesize(1))");
+  return content.replace(/usesize\((.+)\)/g, "calc($1 * var(--size))");
 };
 
 export default loader;
