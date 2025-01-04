b0a4c39511ba91f9dda93ea62038fc57ce958efb
Dominik Żegleń <flesz3@o2.pl>
2025-01-04 12:29:49 +0100

    Improve code readability

--- ui/components/TacticalMap/TacticalMap.tsx
+++ ui/components/TacticalMap/TacticalMap.tsx
@@ -90,7 +90,8 @@ export class TacticalMap extends React.PureComponent<{ sim: Sim }> {
   }
 
   async onControlClick(mousePosition: Vec2, button: MouseButton) {
-    let targetId;
+    let targetId: number | null = null;
+
     if (this.raycastHits.length) {
       let mesh = this.raycastHits[0];
       if (
@@ -99,6 +100,7 @@ export class TacticalMap extends React.PureComponent<{ sim: Sim }> {
       ) {
         mesh = this.raycastHits[1];
       }
+      const isDoubleClick = Date.now() - this.lastClicked < 200;
       // eslint-disable-next-line default-case
       switch (button) {
         case 0:
@@ -106,7 +108,7 @@ export class TacticalMap extends React.PureComponent<{ sim: Sim }> {
           gameStore.setSelectedUnit(this.sim.getOrThrow(mesh.entityId));
           defaultClickSound.play();
 
-          if (Date.now() - this.lastClicked < 200) {
+          if (isDoubleClick) {
             gameStore.focusUnit();
           }
 
@@ -139,91 +141,15 @@ export class TacticalMap extends React.PureComponent<{ sim: Sim }> {
   async onEngineUpdate() {
     if (!(assetLoader.ready && this.engine.isFocused())) return;
 
-    for (const entity of defaultIndexer.renderable.getIt()) {
-      if (entity.cp.position.sector !== gameStore.sector.id) {
-        if (this.meshes.has(entity.id)) {
-          this.meshes.get(entity.id)!.destroy();
-          this.engine.scene.entities.removeChild(this.meshes.get(entity.id)!);
-          this.meshes.delete(entity.id);
-        }
-        continue;
-      }
-      // FIXME: Remove this debug code
-      if (!(entity.cp.render.model in assetLoader.models)) {
-        // eslint-disable-next-line no-console
-        console.log("Missing model:", entity.cp.render.model);
-        if (entity.hasComponents(["dockable"])) {
-          entity.cp.render.model = "ship/dart";
-
-          if (entity.cp.dockable.size === "medium") {
-            entity.cp.render.model = "ship/mCiv";
-          }
-
-          if (entity.cp.dockable.size === "large") {
-            entity.cp.render.model = "ship/lMil";
-          }
-
-          if (entity.cp.model?.slug === "dart") {
-            entity.cp.render.model = "ship/dart";
-          }
-        } else if (entity.hasTags(["facility"])) {
-          if (entity.tags.has("gateway")) {
-            entity.cp.render.model = "facility/gateway";
-          } else {
-            entity.cp.render.model = "facility/default";
-          }
-        }
-      }
-
-      if (!this.meshes.has(entity.id)) {
-        const mesh = new EntityMesh(this.engine, entity);
-        this.engine.scene.entities.addChild(mesh);
-        this.meshes.set(entity.id, mesh);
-      }
-
-      const mesh = this.meshes.get(entity.id)!;
-      mesh.updatePosition();
-    }
-
-    const normalisedMousePos = new Vec2(
-      2.0 * (this.control!.mouse.x / this.engine.gl.renderer.width) - 1.0,
-      2.0 * (1.0 - this.control!.mouse.y / this.engine.gl.renderer.height) - 1.0
-    );
-    this.raycast.castMouse(this.engine.camera, normalisedMousePos);
-    this.raycastHits = this.raycast.intersectBounds([
-      ...this.meshes.values(),
-    ]) as EntityMesh[];
+    this.updateRenderables();
+    this.updateRaycast();
 
     this.engine.canvas.style.cursor = this.raycastHits.length
       ? "pointer"
       : "default";
 
-    const path = this.engine.scene.ui.children.find(
-      (c) => c instanceof Path
-    ) as Path | undefined;
-    if (path && gameStore.selectedUnit) {
-      path.update(
-        Path.getPath(
-          gameStore.selectedUnit.requireComponents(["position", "orders"]),
-          scale
-        )
-      );
-    }
-
-    if (gameStore.unitFocused && gameStore.selectedUnit) {
-      const entity = gameStore.selectedUnit.requireComponents(["position"]);
-      this.control!.lookAt(
-        new Vec3(
-          entity.cp.position.coord[0] * scale,
-          0,
-          entity.cp.position.coord[1] * scale
-        )
-      );
-
-      if (gameStore.sector.id !== entity.cp.position.sector) {
-        gameStore.setSector(this.sim.getOrThrow(entity.cp.position.sector));
-      }
-    }
+    this.updateUIElements();
+    this.updateSelection();
     this.control!.update();
   }
 
@@ -288,6 +214,100 @@ export class TacticalMap extends React.PureComponent<{ sim: Sim }> {
       if (entity.hasComponents(["position", "orders"])) {
         const path = new Path(this.engine);
         this.engine.scene.ui.addChild(path);
+      }
+    }
+  }
+
+  updateRaycast() {
+    const normalisedMousePos = new Vec2(
+      2.0 * (this.control!.mouse.x / this.engine.gl.renderer.width) - 1.0,
+      2.0 * (1.0 - this.control!.mouse.y / this.engine.gl.renderer.height) - 1.0
+    );
+    this.raycast.castMouse(this.engine.camera, normalisedMousePos);
+    this.raycastHits = this.raycast.intersectBounds([
+      ...this.meshes.values(),
+    ]) as EntityMesh[];
+  }
+
+  updateRenderables() {
+    for (const entity of defaultIndexer.renderable.getIt()) {
+      if (entity.cp.position.sector !== gameStore.sector.id) {
+        if (this.meshes.has(entity.id)) {
+          this.meshes.get(entity.id)!.destroy();
+          this.engine.scene.entities.removeChild(this.meshes.get(entity.id)!);
+          this.meshes.delete(entity.id);
+        }
+        continue;
+      }
+      // FIXME: Remove this debug code
+      if (!(entity.cp.render.model in assetLoader.models)) {
+        // eslint-disable-next-line no-console
+        console.log("Missing model:", entity.cp.render.model);
+        if (entity.hasComponents(["dockable"])) {
+          entity.cp.render.model = "ship/dart";
+
+          if (entity.cp.dockable.size === "medium") {
+            entity.cp.render.model = entity.tags.has("role:mining")
+              ? "ship/mMinExample"
+              : entity.tags.has("role:military")
+              ? "ship/axe"
+              : "ship/mCiv";
+          }
+
+          if (entity.cp.dockable.size === "large") {
+            entity.cp.render.model = "ship/lMil";
+          }
+
+          if (entity.cp.model?.slug === "dart") {
+            entity.cp.render.model = "ship/dart";
+          }
+        } else if (entity.hasTags(["facility"])) {
+          if (entity.tags.has("gateway")) {
+            entity.cp.render.model = "facility/gateway";
+          } else {
+            entity.cp.render.model = "facility/default";
+          }
+        }
+      }
+
+      if (!this.meshes.has(entity.id)) {
+        const mesh = new EntityMesh(this.engine, entity);
+        this.engine.scene.entities.addChild(mesh);
+        this.meshes.set(entity.id, mesh);
+      }
+
+      const mesh = this.meshes.get(entity.id)!;
+      mesh.updatePosition();
+    }
+  }
+
+  updateUIElements() {
+    const path = this.engine.scene.ui.children.find(
+      (c) => c instanceof Path
+    ) as Path | undefined;
+    if (path && gameStore.selectedUnit) {
+      path.update(
+        Path.getPath(
+          gameStore.selectedUnit.requireComponents(["position", "orders"]),
+          scale
+        )
+      );
+    }
+  }
+
+  updateSelection() {
+    if (gameStore.unitFocused && gameStore.selectedUnit) {
+      const entity = gameStore.selectedUnit.requireComponents(["position"]);
+      this.control!.lookAt(
+        new Vec3(
+          entity.cp.position.coord[0] * scale,
+          0,
+          entity.cp.position.coord[1] * scale
+        )
+      );
+
+      if (gameStore.sector.id !== entity.cp.position.sector) {
+        gameStore.setSector(this.sim.getOrThrow(entity.cp.position.sector));
       }
     }
   }
