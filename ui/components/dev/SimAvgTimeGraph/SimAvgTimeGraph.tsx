52e8a6450529e2f8019eaf09dc1d3602d46de9f7
Dominik Żegleń <flesz3@o2.pl>
2025-03-14 15:54:49 +0100

    Improve component scaling

--- ui/components/dev/SimAvgTimeGraph/SimAvgTimeGraph.tsx
+++ ui/components/dev/SimAvgTimeGraph/SimAvgTimeGraph.tsx
@@ -1,17 +1,24 @@
 import { frameData } from "@core/systems/reporting/avgFrameReporting";
 import { useObservable } from "@ui/hooks/useObservable";
 import React from "react";
+import { useGameSettings } from "@ui/hooks/useGameSettings";
 import { SimAvgTimeGraphComponent } from "./SimAvgTimeGraphComponent";
 import styles from "./styles.scss";
 
 const SimAvgTimeGraph: React.FC = () => {
   const [data] = useObservable(frameData);
+  const [settings] = useGameSettings();
 
   if (data.length === 0) return null;
 
   return (
     <div className={styles.root}>
-      <SimAvgTimeGraphComponent data={data} height={200} width={600} />
+      <SimAvgTimeGraphComponent
+        data={data}
+        height={200}
+        width={600}
+        scale={settings.scale / 10}
+      />
     </div>
   );
 };
--- ui/components/dev/SimAvgTimeGraph/SimAvgTimeGraphComponent.tsx
+++ ui/components/dev/SimAvgTimeGraph/SimAvgTimeGraphComponent.tsx
@@ -4,6 +4,7 @@ interface GraphProps {
   data: number[];
   width: number;
   height: number;
+  scale: number;
 }
 
 const maxPoints = 31;
@@ -15,47 +16,51 @@ function drawLabel(
   value: number,
   width: number,
   height: number,
-  max: number
+  max: number,
+  scale: number
 ) {
-  const y = height - (value / max) * (height - offsetY);
+  const y = height - (value / max) * (height - offsetY * scale);
   context.beginPath();
-  context.lineWidth = 1;
-  context.moveTo(offsetX, y);
+  context.lineWidth = 1 * scale;
+  context.moveTo(offsetX * scale, y);
   context.lineTo(width, y);
   context.stroke();
 
   context.textAlign = "right";
-  context.fillText(value.toFixed(2), offsetX - 5, y + 4);
+  context.fillText(value.toFixed(2), (offsetX - 5) * scale, y + 4 * scale);
 }
 
 export const SimAvgTimeGraphComponent: React.FC<GraphProps> = ({
   data,
-  width,
-  height,
+  width: widthProp,
+  height: heightProp,
+  scale,
 }) => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const width = widthProp * scale;
+  const height = heightProp * scale;
 
   React.useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas?.getContext("2d");
     const styles = getComputedStyle(canvasRef.current!);
-    const max = Math.max(...data);
+    const max = Math.max(...data, Math.floor(1000 / 240));
 
     if (context) {
       // Clear the canvas
       context.clearRect(0, 0, width, height);
 
       // Calculate the scaling factor
-      const scaleFactor = (height - offsetY) / max;
+      const scaleFactor = (height - offsetY * scale) / max;
 
       // Draw the graph
       context.strokeStyle = styles.getPropertyValue("--palette-primary");
-      context.lineWidth = 2;
+      context.lineWidth = 2 * scale;
       context.moveTo(width, height - data[0] * scaleFactor);
       for (let i = 1; i < data.length; i++) {
         const x =
-          ((width - offsetX) / (maxPoints - 1)) * (maxPoints - i - 1) +
-          offsetX +
+          ((width - offsetX * scale) / (maxPoints - 1)) * (maxPoints - i - 1) +
+          offsetX * scale +
           2;
         context.beginPath();
         context.moveTo(x, height);
@@ -66,22 +71,22 @@ export const SimAvgTimeGraphComponent: React.FC<GraphProps> = ({
       // Draw the axes
       context.beginPath();
       context.strokeStyle = styles.getPropertyValue("--palette-border");
-      context.lineWidth = 1;
-      context.moveTo(offsetX, offsetY - 5);
-      context.lineTo(offsetX, height);
+      context.lineWidth = scale;
+      context.moveTo(offsetX * scale, (offsetY - 5) * scale);
+      context.lineTo(offsetX * scale, height);
       context.lineTo(width, height);
       context.stroke();
 
       context.strokeStyle = styles.getPropertyValue("--palette-text-3");
       context.fillStyle = styles.getPropertyValue("--palette-text-1");
-      context.font = "normal 18px Monospace";
+      context.font = `normal ${18 * scale}px Monospace`;
       [max, (max * 3) / 4, max / 2, max / 4].forEach((value) =>
-        drawLabel(context, value, width, height, max)
+        drawLabel(context, value, width, height, max, scale)
       );
 
       context.fillStyle = styles.getPropertyValue("--palette-primary");
       context.textAlign = "right";
-      context.fillText(data[0].toFixed(2), offsetX - 5, height);
+      context.fillText(data[0].toFixed(2), (offsetX - 5) * scale, height);
     }
   }, [data, width, height]);
 
