8edb516c19269c7057d79520e84c72a00036ea33
Dominik Żegleń <flesz3@o2.pl>
2025-04-14 15:37:59 +0200

    Fix missing settings error

--- ui/hooks/useGameSettings.ts
+++ ui/hooks/useGameSettings.ts
@@ -1,5 +1,17 @@
 import { defaultGameSttings } from "@core/settings";
+import { useMemo } from "react";
+import merge from "lodash/merge";
 import { useLocalStorage } from "./useLocalStorage";
 
-export const useGameSettings = () =>
-  useLocalStorage("gameSettings", defaultGameSttings);
+export function useGameSettings() {
+  const [settingsFromStorage, setSettings] = useLocalStorage(
+    "gameSettings",
+    defaultGameSttings
+  );
+  const settings = useMemo(
+    () => merge({}, defaultGameSttings, settingsFromStorage),
+    [settingsFromStorage]
+  );
+
+  return [settings, setSettings] as const;
+}
